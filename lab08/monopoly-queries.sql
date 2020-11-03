7ad9fc3c93abfb065c7538979cdbfe0d37c3e10b
Jacob Brink <jacobpeterbrink@gmail.com>
2020-11-03 10:43:52 -0500

    Commented out SQL Commands for Turn In

--- lab08/monopoly-queries.sql
+++ lab08/monopoly-queries.sql
@@ -1,54 +1,54 @@
---
+-- --
 -- This SQL script implements sample queries on the Monopoly database.
---
+-- --
 -- @author Jacob Brink
 -- @lab 08
---
+-- --
 
 -- 8.1
 -- 8.1.a Retrieve a list of all the games, ordered by date with the most recent game coming first. 
-SELECT *
-  FROM Game
-  ORDER BY time
-  DESC;
+-- SELECT *
+--   FROM Game
+--   ORDER BY time
+--   DESC;
   
 -- 8.1.b Retrieve all the games that occurred in the past week.
-SELECT *
-  FROM Game
-  WHERE time >= current_date - interval '7' day;
+-- SELECT *
+--   FROM Game
+--   WHERE time >= current_date - interval '7' day;
   
 -- 8.1.c Retrieve a list of players who have (non-NULL) names.
-SELECT *
-  FROM Player
-  WHERE name is NOT NULL;
+-- SELECT *
+--   FROM Player
+--   WHERE name is NOT NULL;
   
 -- 8.1.d Retrieve a list of IDs for players who have some game score larger than 2000.
-SELECT *
-  FROM Player, PlayerGame
-  WHERE Player.ID = PlayerGame.playerID
-  AND PlayerGame.score > 2000;
+-- SELECT *
+--   FROM Player, PlayerGame
+--   WHERE Player.ID = PlayerGame.playerID
+--   AND PlayerGame.score > 2000;
   
 -- 8.1.e Retrieve a list of players who have GMail accounts.
-SELECT * 
-  FROM Player
-  WHERE Player.emailAddress LIKE '%@gmail%';
+-- SELECT * 
+--   FROM Player
+--   WHERE Player.emailAddress LIKE '%@gmail%';
 
 -- 8.2
 -- 8.2.a Retrieve all “The King”’s game scores in decreasing order.
-SELECT score
-  FROM PlayerGame, Player
-  WHERE Player.ID = PlayerGame.playerID
-  AND Player.name = 'The King'
-  ORDER BY score DESC;
+-- SELECT score
+--   FROM PlayerGame, Player
+--   WHERE Player.ID = PlayerGame.playerID
+--   AND Player.name = 'The King'
+--   ORDER BY score DESC;
   
 -- 8.2.b Retrieve the name of the winner of the game played on 2006-06-28 13:20:00. 
-SELECT name, score
-  FROM Player, PlayerGame, Game
-  WHERE Game.time = '2006-06-28 13:20:00'
-  AND Player.ID = PlayerGame.playerID
-  AND PlayerGame.gameID = Game.ID
-  ORDER BY score DESC
-  LIMIT 1;
+-- SELECT name, score
+--   FROM Player, PlayerGame, Game
+--   WHERE Game.time = '2006-06-28 13:20:00'
+--   AND Player.ID = PlayerGame.playerID
+--   AND PlayerGame.gameID = Game.ID
+--   ORDER BY score DESC
+--   LIMIT 1;
   
 -- 8.2.c So what does that P1.ID < P2.ID clause do in the last example query? 
 
